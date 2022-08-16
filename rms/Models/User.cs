ba94e0b8b4850105f599619851938f6937cfdeba
Jihed Larayedh <Larajihade@gmail.com>
2022-08-16 16:18:24 +0100

    Add project files.

---
+++ rms/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace rms.Models
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public object RefreshToken { get; internal set; }
+    }
+}
